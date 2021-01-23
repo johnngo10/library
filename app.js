fe47da9016eb66084a9a3fff35566224161bc548
xuanngo10 <xuan.ngo10@gmail.com>
2021-01-23 12:20:41 -0800

    added UI and styling

---
+++ app.js
@@ -0,0 +1,24 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages}, ${read}`;
+  };
+}
+
+function addBookToLibrary() {}
+
+const theHobbit = new Book(
+  'The Hobbit',
+  'J.R.R Tolkien',
+  '295 pages',
+  'not read yet'
+);
+
+console.log(theHobbit.info());
+
+// If title is too long, do ...
